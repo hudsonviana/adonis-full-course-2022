48f09567e698aab987a131186cc1ac34c4b91c45
hudsonviana <hudson1206@gmail.com>
2023-04-04 23:12:01 -0300

    Request Validation

--- app/Controllers/Http/ArticlesController.ts
+++ app/Controllers/Http/ArticlesController.ts
@@ -1,21 +1,21 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Database from '@ioc:Adonis/Lucid/Database'
+import CreateArticleValidator from 'App/Validators/CreateArticleValidator'
 
 export default class ArticlesController {
-  public async view({ view }) {
+  public async view({ view }: HttpContextContract) {
     const articles = await Database.from('articles').select('*')
     return view.render('article/view', { articles })
   }
 
-  public async create({ view }) {
+  public async create({ view }: HttpContextContract) {
     return view.render('article/create')
   }
 
   public async store({ request, response }: HttpContextContract) {
-    const { title, content, image } = request.body()
-    await Database.table('articles').insert({ title, content, image })
-
-    return response.redirect().back()
+    const payload = await request.validate(CreateArticleValidator)
+    await Database.table('articles').insert(payload)
+    response.redirect().back()
   }
 }
