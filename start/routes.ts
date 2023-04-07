96375e25791360a8b7eb357cf1392e95f0ea2554
hudsonviana <hudson1206@gmail.com>
2023-04-07 18:43:05 -0300

    Login User

--- start/routes.ts
+++ start/routes.ts
@@ -1,7 +1,21 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.group(() => {
+  Route.resource('news', 'ArticlesController').paramFor('news', 'slug')
+}).middleware('auth')
+
 Route.get('/', async ({ view }) => {
   return view.render('welcome')
 })
 
-Route.resource('news', 'ArticlesController').paramFor('news', 'slug')
+Route.get('/login', async ({ view }) => {
+  return view.render('auth/login')
+}).as('auth.signin')
+
+Route.post('login', async ({ auth, request, response }) => {
+  const email = request.input('email')
+  const password = request.input('password')
+
+  await auth.use('web').attempt(email, password)
+  return response.redirect('/')
+}).as('auth.login')
